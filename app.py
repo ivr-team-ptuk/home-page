cc18047c519487337804ab2b404f9301fa20d8eb
TameemKh <tameemkhanfar@gmail.com>
2026-05-22 22:27:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ function toggleNavbar() {{
         <div class="ivr-navbar">
 
             <div class="nav-logo">
-                IVR PDF Tools
+                Black_Square-01.svg
             </div>
 
             <div class="nav-links">
